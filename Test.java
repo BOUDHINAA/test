c4a789eddf460cae7b08d958d2c3a94ce059b38d
Ahmed Boudhina <ahmed.boudhina@esprit.tn>
2023-10-03 23:13:28 +0100

    Initial commit

---
+++ Test.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+public class Test {
+
+	public static void main (String[] args){
+			int n1,n2;
+			System.out.println("Enter a number: ");
+			Scanner s1 = new Scanner(System.in);
+			n1 = s1.nextInt();
+			n2= n1 * 2 ;				
+			System.out.println("The double of "+ n1 +" is "+ n2 +"\n");
+			
+				
+							   }
+
+		}
